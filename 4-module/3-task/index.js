4f26799a19f50d11c707515eb62debd372864638
Isaac Shir <aziksimpson@gmail.com>
2022-11-16 15:10:48 +0300

    highlight function

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,36 @@
 function highlight(table) {
   // ваш код...
+  const tableBody = table.querySelector(`tbody`);
+  const rows = tableBody.rows;
+
+  for (row of rows) {
+    const status = row.cells[3];
+    const gender = row.cells[2];
+    const age = row.cells[1];
+
+    switch (status.dataset.available) {
+    case `true`:
+      row.classList.add(`available`);
+      break;
+    case `false`:
+      row.classList.add(`unavailable`);
+      break;
+    case undefined:
+      row.hidden = true;
+      break;
+    }
+
+    switch (gender.textContent) {
+    case `f`:
+      row.classList.add(`female`);
+      break;
+    case `m`:
+      row.classList.add(`male`);
+      break;
+    }
+
+    if (age.textContent < 18) {
+      row.style.textDecoration = `line-through`;
+    }
+  }
 }
