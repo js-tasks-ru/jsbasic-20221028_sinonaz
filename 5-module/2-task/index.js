8d75e1cacefcfeaa9a8aa241f940e6afaea0b5b2
Isaac Shir <aziksimpson@gmail.com>
2022-11-21 22:54:53 +0300

    toggleText function

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,14 @@
 function toggleText() {
   // ваш код...
+
+  const buttonElement = document.querySelector(`.toggle-text-button`);
+  const textElement = document.querySelector(`#text`);
+
+  buttonElement.addEventListener(`click`, () => {
+    if (textElement.hidden) {
+      textElement.hidden = false;
+    } else {
+      textElement.hidden = true;
+    }
+  });
 }
